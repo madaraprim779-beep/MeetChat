cdb73fcc8bd919ab98608a09d9881df7b2fcf0ab
madaraprim779-beep <madaraprim779@gmail.com>
2026-07-05 01:06:08 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,21 @@
+// 1. Importez les fonctions modulaires nécessaires
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// 2. Votre configuration reste identique
 const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJECT_ID",
-  storageBucket: "TON_BUCKET",
+  apiKey: "VOTRE_API_KEY",
+  authDomain: "VOTRE_PROJET.firebaseapp.com",
+  projectId: "VOTRE_PROJECT_ID",
+  storageBucket: "VOTRE_BUCKET",
   messagingSenderId: "XXXX",
   appId: "XXXX"
 };
 
-firebase.initializeApp(firebaseConfig);
+// 3. Initialisation
+const app = initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+// 4. Exportation des services (C'est ici que ça change)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
