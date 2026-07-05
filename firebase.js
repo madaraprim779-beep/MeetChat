d8788a3b0efa5916a6d546bd77a7b772f73f6be9
madaraprim779-beep <madaraprim779@gmail.com>
2026-07-05 00:10:24 +0000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "TON_API_KEY",
+  authDomain: "TON_PROJET.firebaseapp.com",
+  projectId: "TON_PROJECT_ID",
+  storageBucket: "TON_BUCKET",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
